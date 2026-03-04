992b118a33146f714755690e7c4431e65ab28813
varunsarmab2205 <varunsarmab22@gmail.com>
2026-03-04 06:53:38 +0530

    Add files via upload

---
+++ prime.py
@@ -0,0 +1,15 @@
+num = int(input("Enter number: "))
+is_prime = True
+
+if num <= 1:
+    is_prime = False
+else:
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            is_prime = False
+            break
+
+if is_prime:
+    print("Prime")
+else:
+    print("Not Prime")
