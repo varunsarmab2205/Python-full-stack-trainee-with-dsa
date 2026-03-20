7a56888b779d17aa0312859a291bfff0421fa5c9
VARUN KANDUKURI <varunsarmab22@gmail.com>
2026-03-20 10:27:49 +0530

    Create day3.py

---
+++ day3.py
@@ -0,0 +1,14 @@
+#now lets work on a simple case study using above -->fee calculator
+
+#details of the student
+name = input(" enter service number ")
+print("********************")
+service_number = int(input("enter service number "))
+units_consumed=float(input("enter units consumed"))
+#calculate power bill
+total_amount= units_consumed*1.2
+print("********************")
+print("service_number",service_number)
+print("units_consumed ",units_consumed)
+print("total_amount ",total_amount)
+print("*********************")
