bbcb16a49c2590f0a93fe1c7c0aa6b411925301a
varunsarmab2205 <varunsarmab22@gmail.com>
2026-03-24 13:11:18 +0530

    day8.py

---
+++ day8.py
@@ -0,0 +1,45 @@
+'''String ---> String is a collection of char 
+by " " or "  '' and we can access the using indexing and also slicing . This also immutable
+i could not able to modify
+any = 'Python Programming'
+so = any.replace ("Python","Java")
+print (any[-6])
+#so =  any.replace ("Python", "java")
+#Print (any)
+#print (so)   '''
+
+'''
+a_day = "I'm Varun from Visakhaptnam, Have no experience as im fresher"
+print(f"My name is {a_day[4:9]}")
+print(f"Have no {a_day[37:48]}")
+'''
+
+'''
+a=123
+b=str(a)
+print(len(b))
+'''
+
+'''
+Methods of string
+------------------
+spilt () ---> remove space, and the is in the list [] it will give the seperated thing in each index
+'''
+
+'''
+some = "python is a programming language"
+print(some.upper())
+upper()--->
+
+replace ()--->this is used to replace old str with the new string
+syntax---> variable_name.replace("old string","normal"))'''
+
+'''
+some = "python is a programming language"
+if "python" in some:
+    print("yes")
+else:
+    print ("No")'''
+
+
+
