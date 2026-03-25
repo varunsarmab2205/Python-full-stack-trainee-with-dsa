e1ac4b456838ada628d2359a8dd0db4edc772624
varunsarmab2205 <varunsarmab22@gmail.com>
2026-03-25 13:04:45 +0530

    day9.py

---
+++ Day9.py
@@ -0,0 +1,54 @@
+'''Vowel_con = input ("Enter a lettetr: ")
+if Vowel_con in "AEIOUaeiou":
+     print ("Vow")
+else:
+    print ("COn")'''
+
+'''Time_aday = input ("Enter 24 Hours time: ")
+Parts_ = Time_aday.split(":")
+hours_ = int(Parts_[0])
+min_ = int (Parts_[1])
+if hours_ >=13 and min_ <37:
+    print(f"{Time_aday} convert into {hours_ - 12} : {min_}pm")
+else:
+    print(f"You have entered normal or min are incorrect")
+
+List---> Different datatypes inside the [] ,  which are separated by ,
+Ex--> [1,"Name", [1,2,"Varun"]]
+
+List_1 = [1,2,3,"Python",[1,2,["Python", "Java"],"Language"]]
+print(List_1[4][2][0][3])
+
+Methods of List
+---------------
+append()---this method is used to add new items into list it will only go for
+the last index of the list
+extend().
+
+syntax--> Variable_name.append(item)
+remove()
+pop()
+
+mutable                                            immutable
+can directly modify on that particular variable.I can not modify on that particular variable
+
+
+
+List_2 = [1,2,3,4,5]
+print(List_2)
+List_2.append(67)
+print(List_2)
+List_2.append(57)
+
+List_3 = [23, 56,89]
+List_0..remove ("Python")
+print(List_0)'''
+
+List_4 = [23,"Python",56,89,6]
+
+
+
+
+
+
+
