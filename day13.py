0e2c54ef71f1d6fb830901891539caf2eaa99ce7
varunsarmab2205 <varunsarmab22@gmail.com>
2026-03-29 20:50:24 +0530

    day13 : Task to find vowels,consonants and spaces

---
+++ day13.py
@@ -0,0 +1,18 @@
+paragraph = "Varun Kandukuri is passionate about technology and learning Python."
+
+vowels = ['a','e','i','o','u','A','E','I','O','U']
+
+vowels_count = 0
+consonents_count = 0
+
+for i in paragraph:
+    if i in vowels:
+        vowels_count = vowels_count + 1
+    elif i != " " and i != ".":
+        consonents_count = consonents_count + 1
+
+spaces_count = paragraph.count(" ")
+
+print("Vowels:", vowels_count)
+print("Consonents:", consonents_count)
+print("Spaces:", spaces_count)
