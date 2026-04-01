7b5b6fdc6f1b4764571c3c4aa2651662358b04dc
varunsarmab2205 <varunsarmab22@gmail.com>
2026-04-01 11:37:03 +0530

    day15 : even and odd sum

--- day15practise.py
+++ day15practise.py
@@ -1,13 +1,21 @@
-numbers = [2, 356, 8, 6, 3, 2, 9]
+numbers = [2, 5, 8, 6, 3, 2, 9]
 
 even_list = []
 odd_list = []
 
+even_sum = 0
+odd_sum = 0
+
 for num in numbers:
     if num % 2 == 0:
         even_list.append(num)
+        even_sum += num
     else:
         odd_list.append(num)
+        odd_sum += num
 
 print("Even numbers:", even_list)
+print("Sum of even numbers:", even_sum)
+
 print("Odd numbers:", odd_list)
+print("Sum of odd numbers:", odd_sum)
