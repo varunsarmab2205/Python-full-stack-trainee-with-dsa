c51bcdefe1f8d161bf6270aa54a1a195afa5b0fd
varunsarmab2205 <varunsarmab22@gmail.com>
2026-04-03 13:04:16 +0530

    day18 : Patterns'

---
+++ day18.py
@@ -0,0 +1,36 @@
+'''num = 5
+for j in range (num) :
+    for i in range(j):
+        print("* ",end = " ")
+    print( )    
+
+num = 6
+for j in range (num) :
+    for i in range(j):
+        print(j,end = " ")
+    print( )
+
+num = 7
+for j in range (num) :
+    for i in range(j):
+        print(i,end = " ")
+    print( )
+    
+
+num = 8
+for j in range (num) :
+    for i in range(num-j):
+        print("*",end = " ")
+    print( )
+
+
+#pyramid 
+num = int (input ("enter limit: "))
+for j in range (num) :
+    print(" " * (num - j) , end = " ")
+    for j in range(j+1):
+        print(" i ",end = "  ")
+    print(  )
+    '''
+
+ 
