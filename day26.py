a9e74bc71ccfa4fa4e6256ee94939ac4b36e7437
varunsarmab2205 <varunsarmab22@gmail.com>
2026-04-16 11:46:09 +0530

    day 26 : chatbot

--- day26.py
+++ day26.py
@@ -2,25 +2,47 @@ import pyttsx3
 
 engine = pyttsx3.init()
 
-def speak_text (text) :
-       engine.say(text)
-
-user_text = input("Enter your message: ").lower()
+def speak_text(text):
+    engine.say(text)
 
 name = "User"
-if  "my name is " in user_text:
-     name = user_text.split("my name is") [-1].strip ()
-elif "name is" in user_text:
-     name = user_text.split ("name is") [-1].strip ()
 
-if user_text in  ["hi", "hello", "hello"]:
-     response = "Hello! How can I help you?"
-elif "name" in user_text:
-     response = f"Hello {name}, how can I help you?"
-else:
-     response = "Sorry, I didn't understand that."
-print(response)
-speak_text(response)
-engine.runAndWait ()
+while True:
+    user_text = input("Enter your message: ").lower()
 
-       
+    # Exit condition
+    if user_text in ["exit", "quit", "bye"]:
+        response = "Goodbye! Have a nice day."
+        print(response)
+        speak_text(response)
+        engine.runAndWait()
+        break
+
+    # Name extraction (your logic kept)
+    if "my name is" in user_text:
+        name = user_text.split("my name is")[-1].strip()
+    elif "name is" in user_text:
+        name = user_text.split("name is")[-1].strip()
+
+    # Greeting
+    if user_text in ["hi", "hello"]:
+        response = f"Hello {name}! How can I help you?"
+
+    # When name is mentioned → start conversation flow
+    elif "name" in user_text:
+        response = f"Good morning {name}, what's your plans today?"
+        print(response)
+        speak_text(response)
+        engine.runAndWait()
+
+        # Take next input (plans)
+        plans = input("Your plans: ")
+
+        response = "That sounds great, have fun boss!"
+
+    else:
+        response = "Sorry, I didn't understand that."
+
+    print(response)
+    speak_text(response)
+    engine.runAndWait()
